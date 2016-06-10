576529dd04c6c0aaa77f132a418b20a80ccdf9f2
Jonathan Wight <jwight@mac.com>
2016-06-10 15:35:46 -0700

    Exclude other directories.

--- Package.swift
+++ Package.swift
@@ -3,5 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftUtilities",
     dependencies: [
+    ],
+    exclude: [
+        "Playgrounds", "Supporting Files", "Unit Tests"
     ]
 )
